63599eeba1d56b44e1a2a17b79157f393f03474b
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-03-27 17:35:35 +0300

    add entity and persist decorator for Person class

--- entities/Person.ts
+++ entities/Person.ts
@@ -1,16 +1,26 @@
+import { entity, id, persist } from "../decorators/index";
+
+@entity('people')
 export default class Person {
+    @id
     id: string;
 
+    @persist
     firstName: string;
 
+    @persist
     lastName: string;
 
+    @persist
     email: string;
 
+    @persist
     department: string;
 
+    @persist
     mobileNumber: string;
-    
+
+    @persist
     age: number;
 
 }
